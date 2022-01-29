c4d4f1bfa518601dcdd53c443b2b7cdc21f08ddf
OnGod <estebancbrugal@gmail.com>
2022-01-29 01:25:58 -0500

    Fixed Login, problem was saveCookie() function. We never defined it lol. Added a bit of form validation

--- js/script.js
+++ js/script.js
@@ -1,71 +1,140 @@
-
 // Login function
 // What else do i need to add
 
-const urlBase = 'http://contacts-20.software/LAMPAPI';
+const urlBase = 'http://143.198.109.82/LAMPAPI';
 const extension = 'php';
 
 let userId = 0;
 let firstName = "";
 let lastName = "";
 
+$(document).ready(function() { 
+	Particles.init({
+		selector: '.background',
+		color: '#00B6FF',
+		connectParticles: true
+	});
+ });
+
+
 function doLogin()
+{
+	// Prevents jquery from running multiple times
+	$("#loginForm").off().on("submit" ,function(e) {
+		e.preventDefault()
+		// For some reason this works to validate forms???
+		if($("#loginForm").has('.has-error').length == 0) {
+			userId = 0;
+			firstName = "";
+			lastName = "";
+			
+			let login = document.getElementById("username").value;
+			let password = document.getElementById("password").value;
+			let hash = md5( password );
+			
+			// what does this return
+			// document.getElementById("loginResult").innerHTML = "";
+
+			// There are two tmp variables, causes error
+			let tmp = {login:login,password:password};
+			//let tmp = {login:login,password:hash};
+			let jsonPayload = JSON.stringify( tmp );
+			
+			let url = urlBase + '/Login.' + extension;
+			
+			// xml object to make request to api
+			let xhr = new XMLHttpRequest();
+			xhr.open("POST", url, true);
+			xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+			try
+			{
+				xhr.onreadystatechange = function() 
+				{
+					if (this.readyState == 4 && this.status == 200) //ggot something bacc from api
+					{
+						let jsonObject = JSON.parse( xhr.responseText );
+						userId = jsonObject.id;
+				
+						if( userId < 1 )
+						{		
+							//document.getElementById("loginResult").innerHTML = "User or Password not recorded";
+							return;
+						}
+
+						firstName = jsonObject.firstName;
+						lastName = jsonObject.lastName;
+						
+						saveCookie();
+			
+						window.location.href = "home.html";
+						
+					}
+				};
+				xhr.send(jsonPayload);
+			}
+			catch(err)
+			{
+				console.log(err.message);
+				document.getElementById("loginResult").innerHTML = err.message;
+			}
+		} 
+		return;
+	})
+}
+
+function saveCookie()
+{
+	let minutes = 20;
+	let date = new Date();
+	date.setTime(date.getTime()+(minutes*60*1000));	
+	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+}
+
+function readCookie()
+{
+	userId = -1;
+	let data = document.cookie;
+	let splits = data.split(",");
+	for(var i = 0; i < splits.length; i++) 
+	{
+		let thisOne = splits[i].trim();
+		let tokens = thisOne.split("=");
+		if( tokens[0] == "firstName" )
+		{
+			firstName = tokens[1];
+		}
+		else if( tokens[0] == "lastName" )
+		{
+			lastName = tokens[1];
+		}
+		else if( tokens[0] == "userId" )
+		{
+			userId = parseInt( tokens[1].trim() );
+		}
+	}
+	
+	if( userId < 0 )
+	{
+		window.location.href = "index.html";
+	}
+	else
+	{
+		document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
+	}
+}
+
+function doLogout()
 {
 	userId = 0;
 	firstName = "";
 	lastName = "";
-	
-	let login = document.getElementById("loginName").value;
-	let password = document.getElementById("loginPassword").value;
-	var hash = md5( password );
-	
-	document.getElementById("loginResult").innerHTML = "";
-
-	// There are two tmp variables, causes errror
-	let tmp = {login:login,password:password};
-	// var tmp = {login:login,password:hash};
-	let jsonPayload = JSON.stringify( tmp );
-	
-	let url = urlBase + '/Login.' + extension;
-
-	let xhr = new XMLHttpRequest();
-	xhr.open("POST", url, true);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.onreadystatechange = function() 
-		{
-			if (this.readyState == 4 && this.status == 200) 
-			{
-				let jsonObject = JSON.parse( xhr.responseText );
-				userId = jsonObject.id;
-		
-				if( userId < 1 )
-				{		
-					document.getElementById("loginResult").innerHTML = "User or Password not recorded";
-					return;
-				}
-		
-				firstName = jsonObject.firstName;
-				lastName = jsonObject.lastName;
-
-				saveCookie();
-	
-				window.location.href = "color.html";
-			}
-		};
-		xhr.send(jsonPayload);
-	}
-	catch(err)
-	{
-		document.getElementById("loginResult").innerHTML = err.message;
-	}
-
+	document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+	window.location.href = "index.html";
 }
 
 function doRegister()
 {
-
+	
 }
 
 function doSearch()
@@ -73,8 +142,10 @@ function doSearch()
 
 }
 
+//Adds contact to database
 function doAdd()
 {
+	//let add = ;
 
 }
 
@@ -116,13 +187,7 @@ function submitContactForm() {
 	resetContactFormFields();
 
 	// You should call doAdd here and pass in the values to be saved in the database
+	// doAdd();
 }
 
-window.onload = function() {
-	Particles.init({
-		selector: '.background',
-		color: '#00B6FF',
-		connectParticles: true
-	});
-  };
 
